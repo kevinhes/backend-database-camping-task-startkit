881b91eb2e4d7ebd6d6269fb0d13d2d8db4bbe54
kevinhes <kevinhes1115@gmail.com>
2024-12-05 11:58:50 +0800

    作業 1-1 修正

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -17,12 +17,12 @@
 INSERT INTO
   'USER'( name, email, roll )
 VALUES
-    ('李燕容',`lee2000@hexschooltest.io`,`USER`)
-    ('王小明',`wXlTq@hexschooltest.io`,`USER`)
-    ('肌肉棒子',`muscle@hexschooltest.io`,`USER`)
-    ('好野人',`richman@hexschooltest.io`,`USER`)
-    ('Q太郎',`starplatinum@hexschooltest.io`,`USER`)
-    ('透明人', 'opacity0@hexschooltest.io','USER')
+    ('李燕容',`lee2000@hexschooltest.io`,`USER`),
+    ('王小明',`wXlTq@hexschooltest.io`,`USER`),
+    ('肌肉棒子',`muscle@hexschooltest.io`,`USER`),
+    ('好野人',`richman@hexschooltest.io`,`USER`),
+    ('Q太郎',`starplatinum@hexschooltest.io`,`USER`),
+    ('透明人', 'opacity0@hexschooltest.io','USER');
 
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 
